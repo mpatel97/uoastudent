382435c44e0f58590c3a513a7c83e261a556027a
mpatel97 <patel.mpatel97@gmail.com>
2019-09-02 15:41:04 +1200

    Enabled subject selection

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let isOpen = false;
-
 // Subjects
 function getSubjects(first){
+    document.getElementById("showCourses").innerHTML = "";
     const uri = "https://api.auckland.ac.nz/service/courses/v2/subjects?effectiveYear=2019";
     const xhr= new XMLHttpRequest();
     xhr.open("GET", uri, true);
@@ -13,55 +12,46 @@ function getSubjects(first){
 }
 
 function showSubjects(subjects, first){
-    let tableContent= ""
+    let content= "";
     const addRecord = (record) => {
-        console.log(record);
         if (record.charAt(0).toUpperCase() === first){
-            tableContent += "<div class='col-lg-2 col-md-2 col-sm-6 col-xs-12'>"
-                            + "<button type='submit' class='btn-primary btn-sm'>"  
-                            + record + "</button> </div>";
+            content += "<div class='col-lg-2 col-md-2 col-sm-4 col-xs-4'>"
+                            + "<button type='submit' class='btn-primary btn-sm' onclick='getCourse(\""
+                            + record +"\")'>" + record + "</button> </div>";
         }
     }
     Object.keys(subjects).forEach(addRecord);
-    document.getElementById("showSubjects").innerHTML = tableContent;
+    if (content == "") {content = "No course offered for this selection"}
+    document.getElementById("showSubjects").innerHTML = content;
 }
 
 // Courses Data
-function getCourses() {
-    if (!isOpen) {
-        isOpen = true;
-
-        const uri = "https://api.auckland.ac.nz/service/courses/v2/courses?subject=compsci&size=200";
-        const xhr= new XMLHttpRequest();
-        xhr.open("GET", uri, true);
-        xhr.onload= () => {
-            const resp= JSON.parse(xhr.responseText);
+function getCourse(subject) {
+    const uri = "https://api.auckland.ac.nz/service/courses/v2/courses?subject=" + subject.toLowerCase() + "&size=150";
+    const xhr= new XMLHttpRequest();
+    xhr.open("GET", uri, true);
+    xhr.onload= () => {
+        const resp= JSON.parse(xhr.responseText);
             
-            resp.data.sort((a,b) => {
-                let x = a["catalogNbr"]; let y = b["catalogNbr"];
-                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-            });
+        resp.data.sort((a,b) => {
+            let x = a["catalogNbr"]; let y = b["catalogNbr"];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
 
-            showCourses(resp.data);
-        }
-        xhr.send(null);
+        showCourse(resp.data);
     }
-    else {
-        document.getElementById("showCourses").innerHTML = "";
-        isOpen = false;
-    }
+    xhr.send(null);
 }
 
-function showCourses(courses) {
-
-    let tableContent= ""
+function showCourse(courses) {
+    let content= "";
     const addRecord = (record) => {
-        tableContent += "<div class='col-lg-2 col-md-2 col-sm-6 col-xs-12'>"
+        content += "<div class='col-lg-2 col-md-2 col-sm-6 col-xs-12'>"
                         + "<button type='submit' class='btn-primary btn-sm'>"  
                         + record.subject + " " + record.catalogNbr
                         + "</button> </div>";
 
     }
     courses.forEach(addRecord);
-    document.getElementById("showCourses").innerHTML = tableContent;
+    document.getElementById("showCourses").innerHTML = content;
 }
